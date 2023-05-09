e689a50cda83beb050b633c3a493889f539334da
Eli1280 <eliramel77@gmail.com>
2023-05-09 15:09:27 +0200

    Ajout du texte dans la page CGU

---
+++ src/view/public/CGU.php
@@ -0,0 +1,141 @@
+<section>
+    <div class="title">
+        <h1>Mentions Légales et CGU</h1>
+    </div>
+    <div class="body">
+        <h2>Mentions Légales </h2>
+        <p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>Ce site est édité par la société  Supra Electronic.
+            <br>
+            <br>
+            <br>Siège social : Supra Electronic Paris
+            <br>Téléphone : +33 (0)4.74.00.00.00
+            <br>
+            <br>
+            <br> Informatique et liberté
+            <br>
+            <br>
+            <br>Aucune information personnelle n'est collectée à votre insu. Les informations que vous nous communiquez lors d’une demande de devis, de renseignements ou d’inscription à la newsletter par courrier, par téléphone, par e-mail ou par formulaire sont uniquement destinées au traitement administratif et commercial de votre demande par la société.
+
+            <br>
+            <br> Elles ne font l'objet d'aucune cession à des tiers. Supra Electronic traite les informations qui vous concernent avec la plus grande confidentialité.
+            <br>
+            <br>
+            <br> Conformément à la loi « Informatique et Libertés » n°78-17 du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et d'opposition aux données personnelles vous concernant. Pour l'exercer, il suffit de nous en faire la demande par écrit aux coordonnées suivantes : 28 rue Notre Dame des Champs, 75006 PARIS, France.
+            <br> Droit d'auteur / copyright
+            <br>
+            <br>
+            <br> L'ensemble du contenu du présent site Internet, y compris nom de domaine, marques, logo, texte… est la propriété de la société Supra Electronic, il est protégé par les lois en vigueur de la législation française sur la propriété intellectuelle.
+            <br>
+            <br>
+            <b<br>r>Aucun élément de ce site ne peut être copié, reproduit, détourné ou dénaturé, et ce, sur quelque support que ce soit, sans constituer un acte de contrefaçon au sens des articles L 335-2 et suivants du code de la propriété intellectuelle.
+            <b<br>r>Accès au site
+            <br>
+            <br>
+            <br>  L'utilisateur de ce site reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ce site. Supra Electronic ne saurait être tenu responsable des éléments en dehors de son contrôle et des dommages qui pourraient éventuellement être subis par l'environnement technique de l'utilisateur et notamment, ses ordinateurs, logiciels, équipements réseaux et tout autre matériel utilisé pour accéder ou utiliser le service et/ou les informations.
+            <br>
+            <br>
+            <br>  Il est rappelé que le fait d'accéder ou de se maintenir frauduleusement dans un système informatique, d'entraver ou de fausser le fonctionnement d'un tel système, d'introduire ou de modifier frauduleusement des données dans un système informatique constitue des délits passibles de sanctions pénales.
+            <br>  Limitation de responsabilité
+            <br>
+            <br>
+            <br>  Supra Electronic s'attache à transmettre des informations régulièrement mises à jour, notamment sur les actualités ainsi que sur le contenu des articles. Toutefois des informations erronées ou des omissions peuvent être constatées, suite notamment à des erreurs de saisie ou de mise en page. Si vous en faisiez le constat nous vous invitons à nous le signaler pour que nous puissions procéder à leur rectification.
+            <br>
+            <br>
+            <br>  Nous nous réservons par ailleurs le droit de modifier les informations ou les éventuelles offres commerciales fournies par le présent site, dans le cadre de nos opérations d'actualisation et de mise à jour des données, et ce sans préavis.
+            <br>
+            <br>
+            <br>  Les liens hypertextes mis en oeuvre au sein du présent site Internet, en direction d'autres sites et/ou de pages personnelles et d'une manière générale vers toutes ressources existantes sur Internet, ne sauraient engager la responsabilité de Supra Electronic.
+            <br>	De même que la société Supra Electronic ne pourra en aucune façon être tenue pour responsable des sites ayant un lien hypertexte avec le présent site et décline toute responsabilité quant à leur contenu et à leur utilisation.
+            <br>
+            <br>
+        <h3>Conditions Générale d'Utilisation</h3>
+
+        <br>
+        <br>        Le présent document a pour objet de définir les modalités et conditions dans lesquelles d’une part,  Supra Electronic , ci-après dénommé l’EDITEUR, met à la disposition de ses utilisateurs le site, et les services disponibles sur le site et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.
+        <br>
+        <br>        Toute connexion au site est subordonnée au respect des présentes conditions.
+        <br>
+        <br>        Pour l’utilisateur, le simple accès au site de l’EDITEUR à l’adresse URL  implique l’acceptation de l’ensemble des conditions décrites ci-après.
+        <br>
+        <br>        Propriété intellectuelle
+        <br>
+        <br>        Variante 1
+        <br>
+        <br>        La structure générale du site, ainsi que les textes, graphiques, images, sons et  vidéos la composant, sont la propriété de l'éditeur ou de ses partenaires. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site, par quelque procédé que ce soit, sans l'autorisation préalable et par écrit de  Supra Electronic  et/ou de ses partenaires est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du Code de la propriété intellectuelle.
+        <br>
+        <br>        La marque "Supra Electronic ", est une marque déposée par Supra Electronic .Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de ces marques, de quelque nature que ce soit, est totalement prohibée.
+        <br>
+        <br>        Variante 2
+        <br>
+        <br>        Aucune reproduction, même partielle prévue à l’article L.122-5 du Code de la propriété intellectuelle, ne peut être faite de ce site sans l’autorisation du directeur de publication.
+        <br>
+        <br>        Variante 3
+        <br>
+        <br>        Tous les éléments de ce site, y compris les documents téléchargeables, sont libres de droit. A l’exception de l’iconographie, la reproduction des pages de ce site est autorisée à la condition d’y mentionner la source. Elles ne peuvent être utilisées à des fins commerciales et publicitaires.
+        <br>
+        <br>        Liens hypertextes
+        <br>
+        <br>        Le site Supra Electronic  peut contenir des liens hypertextes vers d’autres sites présents sur le réseau Internet. Les liens vers ces autres ressources vous font quitter le site
+        <br>
+        <br>        Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse de l’EDITEUR. Aucune autorisation ou demande d’information préalable ne peut être exigée par l’éditeur à l’égard d’un site qui souhaite établir un lien vers le site de l’éditeur. Il convient toutefois d’afficher ce site dans une nouvelle fenêtre du navigateur. Cependant, l’EDITEUR se réserve le droit de demander la suppression d’un lien qu’il estime non conforme à l’objet du site
+        <br>
+        <br>        Responsabilité de l’éditeur
+        <br>
+        <br>        Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent de sources considérées comme étant fiables.
+        <br>
+        <br>        Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes techniques et des erreurs typographiques.
+        <br>
+        <br>        L’EDITEUR se réserve le droit de les corriger, dès que ces erreurs sont portées à sa connaissance.
+        <br>
+        <br>        Il est fortement recommandé de vérifier l’exactitude et la pertinence des informations et/ou documents mis à disposition sur ce site.
+        <br>
+        <br>        Les informations et/ou documents disponibles sur ce site sont susceptibles d’être modifiés à tout moment, et peuvent avoir fait l’objet de mises à jour. En particulier, ils peuvent avoir fait l’objet d’une mise à jour entre le moment de leur téléchargement et celui où l’utilisateur en prend connaissance.
+        <br>
+        <br>        L’utilisation des informations et/ou documents disponibles sur ce site se fait sous l’entière et seule responsabilité de l’utilisateur, qui assume la totalité des conséquences pouvant en découler, sans que l’EDITEUR puisse être recherché à ce titre, et sans recours contre ce dernier.
+        <br>
+        <br>        L’EDITEUR ne pourra en aucun cas être tenu responsable de tout dommage de quelque nature qu’il soit résultant de l’interprétation ou de l’utilisation des informations et/ou documents disponibles sur ce site.
+        <br>
+        <br>        Accès au site
+        <br>
+        <br>        L’éditeur s’efforce de permettre l’accès au site 24 heures sur 24, 7 jours sur 7, sauf en cas de force majeure ou d’un événement hors du contrôle de l’EDITEUR, et sous réserve des éventuelles pannes et interventions de maintenance nécessaires au bon fonctionnement du site et des services.
+        <br>
+        <br>        Par conséquent, l’EDITEUR ne peut garantir une disponibilité du site et/ou des services, une fiabilité des transmissions et des performances en terme de temps de réponse ou de qualité. Il n’est prévu aucune assistance technique vis à vis de l’utilisateur que ce soit par des moyens électronique ou téléphonique.
+        <br>
+        <br>        La responsabilité de l’éditeur ne saurait être engagée en cas d’impossibilité d’accès à ce site et/ou d’utilisation des services.
+        <br>
+        <br>        Par ailleurs, l’EDITEUR peut être amené à interrompre le site ou une partie des services, à tout moment sans préavis, le tout sans droit à indemnités. L’utilisateur reconnaît et accepte que l’EDITEUR ne soit pas responsable des interruptions, et des conséquences qui peuvent en découler pour l’utilisateur ou tout tiers.
+        <br>
+        <br>        Modification des conditions d’utilisation
+        <br>
+        <br>        L’EDITEUR se réserve la possibilité de modifier, à tout moment et sans préavis, les présentes conditions d’utilisation afin de les adapter aux évolutions du site et/ou de son exploitation.
+        <br>
+        <br>        Règles d'usage d'Internet
+        <br>
+        <br>        L’utilisateur déclare accepter les caractéristiques et les limites d’Internet, et notamment reconnaît que :
+        <br>
+        <br>        L’EDITEUR n’assume aucune responsabilité sur les services accessibles par Internet et n’exerce aucun contrôle de quelque forme que ce soit sur la nature et les caractéristiques des données qui pourraient transiter par l’intermédiaire de son centre serveur.
+        <br>
+        <br>        L’utilisateur reconnaît que les données circulant sur Internet ne sont pas protégées notamment contre les détournements éventuels. La présence du logo  Supra Electronic institue une présomption simple de validité. La communication de toute information jugée par l’utilisateur de nature sensible ou confidentielle se fait à ses risques et périls.
+        <br>
+        <br>        L’utilisateur reconnaît que les données circulant sur Internet peuvent être réglementées en termes d’usage ou être protégées par un droit de propriété.
+        <br>
+        <br>        L’utilisateur est seul responsable de l’usage des données qu’il consulte, interroge et transfère sur Internet.
+        <br>
+        <br>        L’utilisateur reconnaît que l’EDITEUR ne dispose d’aucun moyen de contrôle sur le contenu des services accessibles sur Internet
+        <br>
+        <br>        Droit applicable
+        <br>
+        <br>        Tant le présent site que les modalités et conditions de son utilisation sont régis par le droit français, quel que soit le lieu d’utilisation. En cas de contestation éventuelle, et après l’échec de toute tentative de recherche d’une solution amiable, les tribunaux français seront seuls compétents pour connaître de ce litige.
+        <br>
+        <br>        Pour toute question relative aux présentes conditions d’utilisation du site, vous pouvez nous écrire à l’adresse suivante :
+        <br>
+        </p>
+        <IMG SRC="" >
+    </div>
+
+</section>
