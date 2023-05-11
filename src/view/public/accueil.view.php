8e1433d00cce2d4814d5b0c6d23cda8fec4698ae
Mattlpt <matthieu.lpt56@gmail.com>
2023-05-11 15:42:37 +0200

    Page d'accueille enrichie en contnenue

--- src/view/public/accueil.view.php
+++ src/view/public/accueil.view.php
@@ -1,42 +1,44 @@
 <body>
-<div class="top-band">
-    <h3 class="text">Nouveauté : le site est maintenant disponible en anglais. Essayer !</h3>
-</div>
-<div class="nav">
+<main>
+    <section id="section-1">
+        <div class="top-band">
+            <h3 class="text">Nouveauté : le site est maintenant disponible en anglais. Essayer !</h3>
+        </div>
+        <div class="nav">
 
-    <i class="uil uil-bars navOpenBtn"></i>
+            <i class="uil uil-bars navOpenBtn"></i>
 
-    <a href="#" class="logo">Supra Electronics</a>
+            <a href="#" class="logo">Supra Electronics</a>
 
-    <ul class="nav-links">
+            <ul class="nav-links">
 
-        <i class="uil uil-times navCloseBtn"></i>
+                <i class="uil uil-times navCloseBtn"></i>
 
-        <li>
-            <a href="/index.php?cible=maincontroleur&fonction=accueil">Accueil</a>
-        </li>
+                <li>
+                    <a href="/index.php?cible=maincontroleur&fonction=accueil">Accueil</a>
+                </li>
 
-        <li>
-            <a href="#">Bracelet</a>
-        </li>
+                <li>
+                    <a href="#">Bracelet</a>
+                </li>
 
-        <li>
-            <a href="#">Capteurs</a>
-        </li>
+                <li>
+                    <a href="#">Capteurs</a>
+                </li>
 
-        <li>
-            <a href="#">Nous contacter</a>
-        </li>
+                <li>
+                    <a href="#">Nous contacter</a>
+                </li>
 
-    </ul>
+            </ul>
 
 
-    <div class="search-box">
-        <i class="uil uil-search search-icon"></i>
-        <input type="text" placeholder="Search here..." />
-    </div>
+            <div class="search-box">
+                <i class="uil uil-search search-icon"></i>
+                <input type="text" placeholder="Search here..." />
+            </div>
 
-    <span class="button">
+            <span class="button">
         <i class="uil uil-search search-icon" id="searchIcon"></i>
         <a class="animated-link-signup" onclick="window.location.href = '/'">
                 <span>Inscription</span>
@@ -46,50 +48,52 @@
                 <span>Connexion</span>
         </a>
     </span>
-</div>
-<div class="page">
-    <p class="intro-title-text">Votre Santé au Travail</p>
-    <p class="intro-title-primary">est au Cœur de nos Préoccupations</p>
+        </div>
+        <div class="page">
+            <p class="intro-title-text">Votre Santé au Travail</p>
+            <p class="intro-title-primary">est au Cœur de nos Préoccupations</p>
 
-    <p class="text-presentation">Inserer ici un texte de présentation - Lorem ipsum dolor sit amet. Et error sunt qui quia totam et rerum assumenda At molestiae tenetur. Quo error galisum sit aspernatur esse ab corrupti obcaecati. Et numquam Quis eos doloribus mollitia eum inventore deserunt in internos consectetur est quae alias nam dolores tempora qui soluta voluptatem.
+            <p class="text-presentation">Inserer ici un texte de présentation - Lorem ipsum dolor sit amet. Et error sunt qui quia totam et rerum assumenda At molestiae tenetur. Quo error galisum sit aspernatur esse ab corrupti obcaecati. Et numquam Quis eos doloribus mollitia eum inventore deserunt in internos consectetur est quae alias nam dolores tempora qui soluta voluptatem.
 
-    </p>
-</div>
+            </p>
+        </div>
+    </section>
 
-<div class="grille">
-    <div class="grid-item">
-        <div class="title-black">Suivez votre <span style="color: var(--error-color)">activité cardiaque</span> tout au long de la journée . . .</div>
-        <div class="text-hard-text">Le coeur peut être mis à rude épreuve lors d'important travaux, suivre l'évolution de ces battements au court de
-            la journée peut être particulièrement utile. <br><br>Lorem ipsum dolor sit amet. Et error sunt qui quia totam et rerum assumenda At molestiae tenetur. Quo error galisum sit aspernatur esse ab corrupti obcaecati. </div>
-    </div>
-    <div class="grid-item">
-        <img src="style/resources/ecg.png" alt="image" height="90%" >
-    </div>
+    <section id="section-2">
+        <div class="vide"></div>
+        <div class="grille">
+            <div class="grid-item">
+                <div class="title-black">Suivez votre <span style="color: var(--error-color)">activité cardiaque</span> tout au long de la journée . . .</div>
+                <div class="text-hard-text">Le coeur peut être mis à rude épreuve lors d'important travaux, suivre l'évolution de ces battements au court de
+                    la journée peut être particulièrement utile. <br><br>Lorem ipsum dolor sit amet. Et error sunt qui quia totam et rerum assumenda At molestiae tenetur. Quo error galisum sit aspernatur esse ab corrupti obcaecati. </div>
+            </div>
+            <div class="grid-item">
+                <img src="style/resources/ecg.png" alt="image" height="90%" >
+            </div>
+        </div>
+    </section>
+
+    <section id="section-3">
+        <div class="vide"></div>
+        <div class="grille">
+            <div class="grid-item">
+                <img src="style/resources/poumon1.jpg" alt="image" height="70%" >
+            </div>
+            <div class="grid-item">
+                <div class="title-black">Prenez soins de <span style="color: var(--succes-color);">vos poumons</span></div>
+                <div class="text-hard-text">Nos capteurs de gaz et particulièrement de CO2 vous permet d'avoir une analyse en temps réel de la qualité de l'aire environnant.
+                    <br><br>
+                    Inserer ici un texte de présentation - Lorem ipsum dolor sit amet. Et error sunt qui quia totam et rerum assumenda At molestiae tenetur. Quo error galisum sit aspernatur esse ab corrupti obcaecati. Et numquam Quis eos doloribus mollitia eum inventore deserunt in internos consectetur est quae alias nam dolores tempora qui soluta voluptatem
+
+            </div>
+        </div>
+    </section>
+</main>
 
 
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
 
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-</div>
 
 
 
 
 </body>
-<script>
-</script>
-
