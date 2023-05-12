6607131fbc51c667fb320176052b2322d6c1c611
Mattlpt <matthieu.lpt56@gmail.com>
2023-05-12 10:58:19 +0200

    normalement le pb de css est fix

--- src/view/public/globalView.php
+++ src/view/public/globalView.php
@@ -6,7 +6,7 @@
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="/style/AccueilPublic.css">
-
+    <link rel="stylesheet" href="/style/login.css">
 </head>
 <body>
 
