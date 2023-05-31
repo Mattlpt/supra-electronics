54bd8961756870c639efbb818a85455632baa3c4
zhouzhou237 <zhouzhou3123@outlook.com>
2023-05-31 17:09:31 +0200

    dashboard

--- src/view/dashboard/Dashboard.view.php
+++ src/view/dashboard/Dashboard.view.php
@@ -1,7 +1,9 @@
 <section class="home">
     <div class="title-text">Dashboard</div>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <link rel="stylesheet" href="style/dashboard.css">
+
     <div class="content-body">
         <p>Nom:_____</p>
         <p>Prenome:_____</p>
@@ -123,7 +125,7 @@
         </div>            
 
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+        
         <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
         <script>
             new Chartist.Line('#traffic-chart', {
