af801fee511c753f6e53bdca7077a82e41f003ee
Mattlpt <matthieu.lpt56@gmail.com>
2023-06-05 21:09:02 +0200

    affichage d'une temérature

--- src/controller/analyse/Stats.php
+++ src/controller/analyse/Stats.php
@@ -9,10 +9,32 @@ class Stats
 
     }
 
+    public static function generateTemperature():array
+    {
+
+
+    }
+
     public static function renderMainView():void
     {
-        renderView("analyse/stats.view.php", NULL, "Stats");
-        print_r('passage');
+
+        $temperatureExterieur = 16;
+        $temperatureSale1 = rand(38, 45);
+        $temperatureSale2 = rand(38, 45);
+        $temperatureSale3 = rand(38, 45);
+
+        $temperature = [1, 2, 3, 4, 5];
+
+        array_unshift($temperature, $temperatureExterieur);
+        array_splice($temperature, 1, 1, $temperatureSale1);
+        array_splice($temperature, 2, 1, $temperatureSale2);
+        array_splice($temperature, 3, 1, $temperatureSale3);
+
+        $vars['temperature'] = $temperature;
+
+        sleep(1);
+
+        renderView("analyse/stats.view.php", $vars, "Stats");
     }
 }
 
--- src/view/analyse/stats.view.php
+++ src/view/analyse/stats.view.php
@@ -2,13 +2,44 @@
     <div class="title-text">Statistiques</div>
     <div class="content-body">
 
-        <div class="carte">
-            <h4>Température</h4>
-            <h6>Machine, Sale 2</h6>
-            <div class="container">
-                <temperature>29 ° C</temperature>
-            </div>
 
+        <div class="grille-4">
+            <div class="grille-4-item">
+                <div class="carte">
+                    <h4>Température</h4>
+                    <h6>Exterieur</h6>
+                    <div class="container">
+                        <temperature><?php echo $temperature = $vars['temperature'][0] ?> ° C</temperature>
+                    </div>
+                </div>
+            </div>
+            <div class="grille-4-item">
+                <div class="carte">
+                    <h4>Température</h4>
+                    <h6>Machine, Sale 1</h6>
+                    <div class="container">
+                        <temperature> <span id="randomNumber"></span> ° C</temperature>
+                    </div>
+                </div>
+            </div>
+            <div class="grille-4-item">
+                <div class="carte">
+                    <h4>Température</h4>
+                    <h6>Machine, Sale 2</h6>
+                    <div class="container">
+                        <temperature><?php echo $temperature = $vars['temperature'][2] ?> ° C</temperature>
+                    </div>
+                </div>
+            </div>
+            <div class="grille-4-item">
+                <div class="carte">
+                    <h4>Température</h4>
+                    <h6>Machine, Sale 3</h6>
+                    <div class="container">
+                        <temperature><?php echo $temperature = $vars['temperature'][3] ?> ° C</temperature>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div>
@@ -51,10 +82,31 @@
     </div>
 </section><!-- Contenu principal -->
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        // Fonction pour charger le nombre aléatoire toutes les secondes
+        function loadRandomNumber() {
+            $.ajax({
+                url: 'randomNumber.php', // Chemin vers le script PHP qui génère le nombre aléatoire
+                success: function(data) {
+                    $('#randomNumber').text(data); // Mettre à jour le contenu de l'élément span avec le nombre aléatoire
+                }
+            });
+        }
+
+        // Charger le nombre aléatoire initial au chargement de la page
+        loadRandomNumber();
+
+        // Actualiser le nombre aléatoire toutes les secondes
+        setInterval(loadRandomNumber, 1000);
+    });
+</script>
+
 <style>
     .carte{
-        width: 20%;
-        height: 20%;
+
         border: 1px solid #ccc;
         border-radius: 5px;
         padding: 10px;
@@ -83,6 +135,22 @@
         font-family: "Poppins", sans-serif;
         font-weight: 600;
         font-size: 35px;
+
+    }
+
+    .grille-4{
+        max-width: 2100px;
+        width: 100%;
+        margin-top: 40px;
+        margin-right: 2px;
+        margin-left: 2px;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 250px);
+        justify-content: center;
+        grid-gap: 10px;
+    }
+
+    .grille-4-item{
 
     }
 </style>
