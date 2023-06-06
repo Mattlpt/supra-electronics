f63900004e9389d1f85cf1f97f56cfef7f6ed6f6
Mattlpt <matthieu.lpt56@gmail.com>
2023-06-06 13:06:28 +0200

    correction analyse et stat

--- src/controller/analyse/Analytics.php
+++ src/controller/analyse/Analytics.php
@@ -9,20 +9,6 @@ class Analytics{
     public static function renderMainView():void
     {
 
-        $temperatureExterieur = 16;
-        $temperatureSale1 = rand(38, 45);
-        $temperatureSale2 = rand(38, 45);
-        $temperatureSale3 = rand(38, 45);
-
-        $temperature = [1, 2, 3, 4, 5];
-
-        array_unshift($temperature, $temperatureExterieur);
-        array_splice($temperature, 1, 1, $temperatureSale1);
-        array_splice($temperature, 2, 1, $temperatureSale2);
-        array_splice($temperature, 3, 1, $temperatureSale3);
-
-        $vars['temperature'] = $temperature;
-
         renderView("analyse/analytics.view.php", $vars, "Analytics");
     }
 }
